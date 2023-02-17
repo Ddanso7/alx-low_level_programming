74795888e1d0affe9261e6a1bcc3769111b537d3
myusername <myemail@gmail.com>
2023-02-16 19:12:07 -0800

    5 printf c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int man(void)
+{
+	prinf("with proper grammar,but the outcome is a piece of art,\n");
+	return(0);
+}
